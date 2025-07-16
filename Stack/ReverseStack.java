6ffe47808e447958a74f7c4b71e0b2d1eb89bf02
Yash Singh <yashsinghss047@gmail.com>
2025-07-16 16:52:32 +0530

    This Program revers stack.

---
+++ Stack/ReverseStack.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class ReverseStack {
+    public static String reverseStack(String s){
+        Stack<Character> ch =  new Stack<>();
+        int indx = 0;
+        while(indx < s.length()){
+            ch.push(s.charAt(indx));
+            indx++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!ch.isEmpty()){
+            char curr = ch.pop();
+            sb.append(curr);
+        }
+        return sb.toString();
+    }
+    public static void main(String args[]){
+        String str = "abc";
+        System.out.println(reverseStack(str));
+    }
+}
