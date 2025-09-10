370c38d805ab1533dd7823fcb28e7527836bc671
Yash Singh <yashsinghss047@gmail.com>
2025-09-10 12:45:26 +0530

    Queue

---
+++ Queue/NonRepeatingLetter.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class NonRepeatingLetter {
+    public static void printNonRepeating(String s){
+        int freq[] = new int[26] ;//'a-z'
+        Queue<Character> q = new LinkedList<>();
+
+        for(int i = 0; i<s.length();i++){
+            char ch = s.charAt(i);
+            q.add(ch);
+            freq[ch-'a']++;
+
+            while(!q.isEmpty() && freq[q.peek()-'a']>1){
+                q.remove();
+            }
+
+            if(q.isEmpty()){
+                System.out.print(-1+" ");
+            }else{
+                System.out.print(q.peek()+" ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        String str = "aabccxb";
+        printNonRepeating(str);
+    }
+}
