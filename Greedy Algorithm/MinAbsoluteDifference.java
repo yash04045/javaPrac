0684122d3bb3b080de6352b012956b81d9e00ff8
Yash Singh <yashsinghss047@gmail.com>
2025-09-13 16:12:45 +0530

    Finding Minimum Absolute Difference Using Greedy Approach.

---
+++ Greedy Algorithm/MinAbsoluteDifference.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class MinAbsoluteDifference {
+    public static void main(String args[]){
+        int A[] = {4, 1, 8, 7};
+        int B[] = {2, 3, 5, 6};
+        Arrays.sort(A);
+        Arrays.sort(B);    
+        
+        int minDiff = 0;
+        for(int i = 0; i<A.length; i++){
+            minDiff += Math.abs(A[i] - B[i]);
+        }
+        System.out.println("Minimum Absolute Difference: " + minDiff);
+    }    
+}
