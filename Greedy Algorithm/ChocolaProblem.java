8b9555ec3179532fc051f14ddccd63c68f06f9b6
Yash Singh <yashsinghss047@gmail.com>
2025-09-15 16:42:45 +0530

    Very Important Problem, Logic is prominent.

---
+++ Greedy Algorithm/ChocolaProblem.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class ChocolaProblem {
+    public static void main(String args[]){
+        int n = 4, m = 6;
+        Integer costVer[] = {2,1,3,1,4}; //m-1
+        Integer costHor[] = {4,1,2}; //n-1
+
+        Arrays.sort(costVer, Collections.reverseOrder());
+        Arrays.sort(costHor, Collections.reverseOrder());
+
+        int h = 0;
+        int  v = 0;
+        int hp = 1;
+        int vp = 1;
+        int cost = 0;
+
+        while(v < costVer.length && h < costHor.length){
+            if(costVer[v] <= costHor[h]){
+                cost += (costHor[h]*vp);
+                hp++;
+                h++;
+            }else{
+                cost += (costVer[v]*hp);
+                vp++;
+                v++;
+            }
+        }
+
+        while(h < costHor.length){
+            cost += (costHor[h]*vp);
+            hp++;
+            h++;
+        }
+
+        while(v < costVer.length){
+            cost += (costVer[v]*hp);
+            vp++;
+            v++;
+        }
+
+        System.out.println("the minimum cost of total cuts is : "+cost);
+    }
+    
+}
