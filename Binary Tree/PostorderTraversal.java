423703874e54e2cebb147065244ee931c49c57b3
Yash Singh <yashsinghss047@gmail.com>
2025-09-22 13:37:15 +0530

    Binary Tree.

---
+++ Binary Tree/PostorderTraversal.java
@@ -0,0 +1,48 @@
+
+
+public class PostorderTraversal {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree{
+        static int indx = -1;
+        public static Node buildTree(int node[]){
+            indx++;
+            if(node[indx] == -1){
+                return null;
+            }
+
+            Node newNode = new Node(node[indx]);
+            newNode.left = buildTree(node);
+            newNode.right = buildTree(node);
+
+            return newNode;
+        }
+
+        public static void postOrder(Node root){
+            if(root == null){
+                return;
+            }
+            postOrder(root.left);
+            postOrder(root.right);           
+            System.out.print(root.data + " ");
+
+        }
+    }
+    public static void main(String args[]){
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTree tree = new BinaryTree();
+
+        Node root = tree.buildTree(nodes);
+        tree.postOrder(root);
+    }
+}
